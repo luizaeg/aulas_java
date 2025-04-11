c23e242258ff67776a1b43ba8de82f7abfbc9cdf
Luiza_Gonck <luizae.goncalves@hotmail.com>
2025-04-11 11:16:31 -0300

    :tada: Commit Inicial

---
+++ helloword/src/helloword/HelloWord.java
@@ -0,0 +1,11 @@
+package helloword;
+
+public class HelloWord {
+	
+	public static void main (String[] args) {
+		
+		System.out.println("Hello Word!");
+		System.out.println("A Turma Java 82 é massa!");
+	}
+
+}
